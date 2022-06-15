fdc8b10b016caaae7700d1e1a655616dee7fe792
Moh0j0jo <pinnel.peter@gmail.com>
2022-06-15 22:08:36 +0200

    testing 1.0

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -2,6 +2,11 @@ const list = ["rock", "paper", "scissors"]
 const min = 0
 const max = 2
 
+
+var playerScore = 0
+var computerScore = 0
+
+
 function randomListI(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }
@@ -12,22 +17,52 @@ function computerSelection() {
 }
 
 function playerSelection() {
-    console.log("Please Choose 0 - Rock, 1 - Paper, 2 - Scissors");
-    let playerHand = parseInt(prompt("Enter a Value", "0"));
-    return list[playerHand]
+    let playerHandID = parseInt(prompt(" Please Choose 0 - Rock, 1 - Paper, 2 - Scissors !"))
+    let playerHand = list[playerHandID]
+    return playerHand
 }
+
+function checkScores(playerScore, computerScore) {
+
+    if (playerScore > computerScore) {
+
+        console.log("You win! Your Score: " + playerScore + " Computer Score: " + computerScore);
+
+    } else if (playerScore < computerScore) {
+
+        console.log("You Loose! Your Score: " + playerScore + " Computer Score: " + computerScore);
+    }
+}
+
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection === computerSelection) {
+    if (playerHand === computerHand) {
+        return playerScore, computerScore;
+   
+    } else if (
+        (playerHand === list[0] && computerHand === list[2]) ||
+        (playerHand === list[1] && computerHand === list[0]) ||
+        (playerHand === list[2] && computerHand === list[1])
+    ) {
 
-        return "Draw, nobody wins!";
-
-    } else if ((playerSelection === list[0] && computerSelection === list[2]) || (playerSelection === list[1] && computerSelection === list[0]) || (playerSelection === list[2] && computerSelection === list[1])) {
-
-        return "You won!";
+        playerScore += 1;
+        return playerScore;
 
     } else {
 
-        return "You loose!";
+        computerScore += 1;
+        return computerScore;
+
     }
+}
+
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+
+        playRound();
+        console.log("Your Score: " + playerScore + " Computer Score: " + computerScore)
+    }
+
+    console.log(checkScores(playerScore, computerScore));
 }
